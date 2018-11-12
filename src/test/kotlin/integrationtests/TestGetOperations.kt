8f129ea9636adaee6ce6adc6a1b70a72072afd81
Neeraj <neeraj.sidhaye@capgemini.com>
2018-11-12 22:13:51 +0000

    integration tests

---
+++ src/test/kotlin/integrationtests/TestGetOperations.kt
@@ -0,0 +1,21 @@
+package integrationtests
+
+import org.jsmart.zerocode.core.domain.TargetEnv
+import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner
+import org.junit.runner.RunWith
+import org.junit.Test
+import org.jsmart.zerocode.core.domain.JsonTestCase
+import org.jsmart.zerocode.core.domain.EnvProperty
+
+
+@TargetEnv("application_host.properties")
+@RunWith(ZeroCodeUnitRunner::class)
+class TestGetOperations {
+	
+	 @Test
+	 @JsonTestCase("integration_tests/get_all_runners.json")
+	 fun `get all runners`(){
+		 
+		}
+	
+}
